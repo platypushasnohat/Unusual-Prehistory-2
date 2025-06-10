1dabb251db9804bfe74bda14c822c41d682c3da5
platypushasnohat <58866828+platypushasnohat@users.noreply.github.com>
2025-06-10 17:44:54 -0600

    Update GinkgoFoliagePlacer.java
    improved generation

--- src/main/java/com/unusualmodding/unusual_prehistory/worldgen/feature/GinkgoFoliagePlacer.java
+++ src/main/java/com/unusualmodding/unusual_prehistory/worldgen/feature/GinkgoFoliagePlacer.java
@@ -29,110 +29,80 @@ public class GinkgoFoliagePlacer extends FoliagePlacer {
         BlockPos blockPos = treeNode.pos();
         BlockPos.MutableBlockPos mutable = blockPos.mutable();
 
-        for (int l = -11; l <= foliageHeight; ++l) {
-
-            if (l == -11) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, l, treeNode.doubleTrunk());
+        for (int height = -11; height <= foliageHeight; ++height) {
+            if (height == -11) {
+                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, height, treeNode.doubleTrunk());
+                if (random.nextBoolean()) {
+                    this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, height - 1, treeNode.doubleTrunk());
+                }
             }
-            if (l == -10) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, l, treeNode.doubleTrunk());
-                mutable.setWithOffset(blockPos, 0, l, 0);
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, -1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, -1));
-
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(2, 0, 0));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-2, 0, 0));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 0, 2));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 0, -2));
+            if (height == -10 || height == -7 || height == -5 || height == -2) {
+                this.generateDiamond(level, placer, random, config, blockPos, 2, height, treeNode.doubleTrunk());
             }
-            if (l == -9) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 2, l, treeNode.doubleTrunk());
-                mutable.setWithOffset(blockPos, 0, l, 0);
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(2, 0, 2));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-2, 0, 2));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(2, 0, -2));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-2, 0, -2));
+            if (height == -9 || height == -6 || height == -4) {
+                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 2, height, treeNode.doubleTrunk());
+                mutable.setWithOffset(blockPos, 0, height, 0);
+                if (random.nextBoolean()) {
+                    tryPlaceLeaf(level, placer, random, config, mutable.offset(2, 0, 2));
+                }
+                if (random.nextBoolean()) {
+                    tryPlaceLeaf(level, placer, random, config, mutable.offset(-2, 0, 2));
+                }
+                if (random.nextBoolean()) {
+                    tryPlaceLeaf(level, placer, random, config, mutable.offset(2, 0, -2));
+                }
+                if (random.nextBoolean()) {
+                    tryPlaceLeaf(level, placer, random, config, mutable.offset(-2, 0, -2));
+                }
             }
-            if (l == -8) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 2, l, treeNode.doubleTrunk());
+            if (height == -8 || height == -6 || height == -3) {
+                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 2, height, treeNode.doubleTrunk());
             }
-            if (l == -7) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, l, treeNode.doubleTrunk());
-                mutable.setWithOffset(blockPos, 0, l, 0);
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, -1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, -1));
-
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(2, 0, 0));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-2, 0, 0));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 0, 2));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 0, -2));
+            if (height == -1) {
+                this.generateDiamond(level, placer, random, config, blockPos, 1, height, treeNode.doubleTrunk());
             }
-            if (l == -6) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 2, l, treeNode.doubleTrunk());
+            if (height == 0) {
+                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, height, treeNode.doubleTrunk());
             }
-            if (l == -5) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, l, treeNode.doubleTrunk());
-                mutable.setWithOffset(blockPos, 0, l, 0);
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, -1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, -1));
-
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(2, 0, 0));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-2, 0, 0));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 0, 2));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 0, -2));
-            }
-            if (l == -4) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 2, l, treeNode.doubleTrunk());
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 2, l + 1, treeNode.doubleTrunk());
-            }
-            if (l == -3) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, l, treeNode.doubleTrunk());
-                mutable.setWithOffset(blockPos, 0, l, 0);
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, -1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, -1));
-
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(2, 0, 0));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-2, 0, 0));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 0, 2));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 0, -2));
-            }
-            if (l == -2) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, l, treeNode.doubleTrunk());
-                mutable.setWithOffset(blockPos, 0, l, 0);
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, -1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, -1));
-
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(2, 0, 0));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-2, 0, 0));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 0, 2));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 0, -2));
-            }
-            if (l == -1) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, l, treeNode.doubleTrunk());
-                mutable.setWithOffset(blockPos, 0, l, 0);
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, 1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(1, 0, -1));
-                tryPlaceLeaf(level, placer, random, config, mutable.offset(-1, 0, -1));
-            }
-            if (l == 0) {
-                this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, l, treeNode.doubleTrunk());
-            }
-            if (l == 1) {
-                mutable.setWithOffset(blockPos, 0, l, 0);
+            if (height == 1) {
+                mutable.setWithOffset(blockPos, 0, height, 0);
                 tryPlaceLeaf(level, placer, random, config, mutable);
+                if (random.nextBoolean()) {
+                    tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 1, 0));
+                } else if (random.nextBoolean()) {
+                    this.placeLeavesRow(level, placer, random, config, treeNode.pos(), 1, height, treeNode.doubleTrunk());
+                    tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 1, 0));
+                    if (random.nextBoolean()) {
+                        tryPlaceLeaf(level, placer, random, config, mutable.offset(0, 2, 0));
+                    }
+                }
             }
         }
+    }
+
+    protected void generateDiamond(LevelSimulatedReader world, FoliageSetter placer, RandomSource random, TreeConfiguration config, BlockPos centerPos, int radius, int y, boolean giantTrunk) {
+        int i = giantTrunk ? 1 : 0;
+        BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
+
+        for (int j = -radius; j <= radius + i; ++j) {
+            for (int k = -radius; k <= radius + i; ++k) {
+                if (!this.validateDiamond(random, j, y, k, radius, giantTrunk)) {
+                    mutable.setWithOffset(centerPos, j, y, k);
+                    tryPlaceLeaf(world, placer, random, config, mutable);
+                }
+            }
+        }
+    }
+
+    protected boolean validateDiamond(RandomSource random, int dx, int y, int dz, int radius, boolean giantTrunk) {
+        if (giantTrunk) {
+            dx = Math.min(Math.abs(dx), Math.abs(dx - 1));
+            dz = Math.min(Math.abs(dz), Math.abs(dz - 1));
+        } else {
+            dx = Math.abs(dx);
+            dz = Math.abs(dz);
+        }
+        return (dx > 1 || dz > 1) && dx != 0 && dz != 0;
     }
 
     public int foliageHeight(RandomSource random, int trunkHeight, TreeConfiguration config) {
