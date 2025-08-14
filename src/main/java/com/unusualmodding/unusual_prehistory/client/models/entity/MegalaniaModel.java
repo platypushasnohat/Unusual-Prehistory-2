9d96bd6f2d06ab6f3994f4c1d10ea0f4ae31e1a9
platypushasnohat <58866828+platypushasnohat@users.noreply.github.com>
2025-08-14 10:58:23 -0600

    megalania stuff

--- src/main/java/com/unusualmodding/unusual_prehistory/client/models/entity/MegalaniaModel.java
+++ src/main/java/com/unusualmodding/unusual_prehistory/client/models/entity/MegalaniaModel.java
@@ -14,25 +14,20 @@ public class MegalaniaModel<T extends Megalania> extends HierarchicalModel<T> {
 	private final ModelPart root;
 	private final ModelPart body_main;
 	private final ModelPart body_upper;
-	private final ModelPart body_overlay;
 	private final ModelPart body;
-	private final ModelPart neck_overlay;
 	private final ModelPart neck;
-	private final ModelPart head_overlay;
 	private final ModelPart head;
-	private final ModelPart jaw_overlay;
 	private final ModelPart jaw;
 	private final ModelPart tongue;
-	private final ModelPart tail1_overlay;
 	private final ModelPart tail1;
-	private final ModelPart tail2_overlay;
 	private final ModelPart tail2;
-	private final ModelPart tail3_overlay;
 	private final ModelPart tail3;
+	private final ModelPart arm_control;
 	private final ModelPart left_arm1;
 	private final ModelPart left_arm2;
 	private final ModelPart right_arm1;
 	private final ModelPart right_arm2;
+	private final ModelPart leg_control;
 	private final ModelPart left_leg1;
 	private final ModelPart left_leg2;
 	private final ModelPart right_leg1;
@@ -42,71 +37,88 @@ public class MegalaniaModel<T extends Megalania> extends HierarchicalModel<T> {
 		this.root = root.getChild("root");
 		this.body_main = this.root.getChild("body_main");
 		this.body_upper = this.body_main.getChild("body_upper");
-		this.body_overlay = this.body_upper.getChild("body_overlay");
-		this.body = this.body_overlay.getChild("body");
-		this.neck_overlay = this.body.getChild("neck_overlay");
-		this.neck = this.neck_overlay.getChild("neck");
-		this.head_overlay = this.neck.getChild("head_overlay");
-		this.head = this.head_overlay.getChild("head");
-		this.jaw_overlay = this.head.getChild("jaw_overlay");
-		this.jaw = this.jaw_overlay.getChild("jaw");
+		this.body = this.body_upper.getChild("body");
+		this.neck = this.body.getChild("neck");
+		this.head = this.neck.getChild("head");
+		this.jaw = this.head.getChild("jaw");
 		this.tongue = this.jaw.getChild("tongue");
-		this.tail1_overlay = this.body.getChild("tail1_overlay");
-		this.tail1 = this.tail1_overlay.getChild("tail1");
-		this.tail2_overlay = this.tail1.getChild("tail2_overlay");
-		this.tail2 = this.tail2_overlay.getChild("tail2");
-		this.tail3_overlay = this.tail2.getChild("tail3_overlay");
-		this.tail3 = this.tail3_overlay.getChild("tail3");
-		this.left_arm1 = this.body_upper.getChild("left_arm1");
+		this.tail1 = this.body.getChild("tail1");
+		this.tail2 = this.tail1.getChild("tail2");
+		this.tail3 = this.tail2.getChild("tail3");
+		this.arm_control = this.body_upper.getChild("arm_control");
+		this.left_arm1 = this.arm_control.getChild("left_arm1");
 		this.left_arm2 = this.left_arm1.getChild("left_arm2");
-		this.right_arm1 = this.body_upper.getChild("right_arm1");
+		this.right_arm1 = this.arm_control.getChild("right_arm1");
 		this.right_arm2 = this.right_arm1.getChild("right_arm2");
-		this.left_leg1 = this.body_main.getChild("left_leg1");
+		this.leg_control = this.body_main.getChild("leg_control");
+		this.left_leg1 = this.leg_control.getChild("left_leg1");
 		this.left_leg2 = this.left_leg1.getChild("left_leg2");
-		this.right_leg1 = this.body_main.getChild("right_leg1");
+		this.right_leg1 = this.leg_control.getChild("right_leg1");
 		this.right_leg2 = this.right_leg1.getChild("right_leg2");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
+
 		PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+
 		PartDefinition body_main = root.addOrReplaceChild("body_main", CubeListBuilder.create(), PartPose.offset(0.0F, -13.5F, 0.0F));
+
 		PartDefinition body_upper = body_main.addOrReplaceChild("body_upper", CubeListBuilder.create(), PartPose.offset(0.0F, 3.5F, 9.5F));
-		PartDefinition body_overlay = body_upper.addOrReplaceChild("body_overlay", CubeListBuilder.create(), PartPose.offset(0.0F, -3.5F, -9.5F));
-		PartDefinition body = body_overlay.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-9.5F, -7.5F, -15.5F, 19.0F, 15.0F, 31.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-		PartDefinition neck_overlay = body.addOrReplaceChild("neck_overlay", CubeListBuilder.create(), PartPose.offset(0.0F, -6.5F, -15.5F));
-		PartDefinition neck = neck_overlay.addOrReplaceChild("neck", CubeListBuilder.create().texOffs(57, 48).addBox(-3.0F, 0.0F, -13.0F, 6.0F, 11.0F, 13.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-		PartDefinition head_overlay = neck.addOrReplaceChild("head_overlay", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, -5.0F));
-		PartDefinition head = head_overlay.addOrReplaceChild("head", CubeListBuilder.create().texOffs(90, 111).addBox(-3.5F, 0.0F, -16.0F, 7.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(70, 0).addBox(-4.5F, 0.0F, -8.0F, 9.0F, 3.0F, 8.0F, new CubeDeformation(0.01F)).texOffs(54, 87).addBox(-2.5F, 2.5F, -15.0F, 5.0F, 1.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(28, 114).addBox(-4.5F, 3.0F, -4.0F, 9.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-		PartDefinition jaw_overlay = head.addOrReplaceChild("jaw_overlay", CubeListBuilder.create(), PartPose.offset(0.0F, 3.0F, -4.0F));
-		PartDefinition jaw = jaw_overlay.addOrReplaceChild("jaw", CubeListBuilder.create().texOffs(60, 111).addBox(-3.5F, 0.0F, -12.0F, 7.0F, 4.0F, 8.0F, new CubeDeformation(0.01F)).texOffs(54, 96).addBox(-2.5F, -0.5F, -11.0F, 5.0F, 1.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(6, 70).addBox(-4.5F, 0.0F, -4.0F, 9.0F, 4.0F, 4.0F, new CubeDeformation(0.01F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-		PartDefinition tongue = jaw.addOrReplaceChild("tongue", CubeListBuilder.create().texOffs(17, 47).addBox(-1.5F, -0.01F, -9.0F, 3.0F, 0.0F, 9.0F, new CubeDeformation(0.0025F)), PartPose.offset(0.0F, 0.0F, -1.0F));
-		PartDefinition tail1_overlay = body.addOrReplaceChild("tail1_overlay", CubeListBuilder.create(), PartPose.offset(0.0F, 2.5F, 15.5F));
-		PartDefinition tail1 = tail1_overlay.addOrReplaceChild("tail1", CubeListBuilder.create().texOffs(0, 46).addBox(-5.5F, -4.0F, -4.0F, 11.0F, 7.0F, 33.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-		PartDefinition tail2_overlay = tail1.addOrReplaceChild("tail2_overlay", CubeListBuilder.create(), PartPose.offset(0.0F, 0.5F, 29.0F));
-		PartDefinition tail2 = tail2_overlay.addOrReplaceChild("tail2", CubeListBuilder.create().texOffs(60, 86).addBox(-4.0F, -2.5F, 0.0F, 8.0F, 5.0F, 20.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-		PartDefinition tail3_overlay = tail2.addOrReplaceChild("tail3_overlay", CubeListBuilder.create(), PartPose.offset(0.0F, 0.5F, 20.0F));
-		PartDefinition tail3 = tail3_overlay.addOrReplaceChild("tail3", CubeListBuilder.create().texOffs(0, 86).addBox(-3.0F, -2.0F, 0.0F, 6.0F, 4.0F, 24.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-		PartDefinition left_arm1 = body_upper.addOrReplaceChild("left_arm1", CubeListBuilder.create().texOffs(71, 12).addBox(-3.0F, -2.0F, -3.0F, 6.0F, 12.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(9.5F, 0.0F, -19.0F));
-		PartDefinition left_arm2 = left_arm1.addOrReplaceChild("left_arm2", CubeListBuilder.create().texOffs(1, 56).addBox(-3.5F, 0.0F, -5.0F, 8.0F, 3.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.5F, 9.0F, -1.0F));
-		PartDefinition right_arm1 = body_upper.addOrReplaceChild("right_arm1", CubeListBuilder.create().texOffs(71, 12).mirror().addBox(-3.0F, -2.0F, -3.0F, 6.0F, 12.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-9.5F, 0.0F, -19.0F));
-		PartDefinition right_arm2 = right_arm1.addOrReplaceChild("right_arm2", CubeListBuilder.create().texOffs(1, 56).mirror().addBox(-4.5F, 0.0F, -5.0F, 8.0F, 3.0F, 7.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-0.5F, 9.0F, -1.0F));
-		PartDefinition left_leg1 = body_main.addOrReplaceChild("left_leg1", CubeListBuilder.create().texOffs(4, 10).addBox(-3.0F, -2.0F, -3.0F, 6.0F, 12.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(9.5F, 3.5F, 9.5F));
+
+		PartDefinition body = body_upper.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-9.5F, -7.5F, -15.5F, 19.0F, 15.0F, 31.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -3.5F, -9.5F));
+
+		PartDefinition neck = body.addOrReplaceChild("neck", CubeListBuilder.create().texOffs(89, 46).addBox(-3.0F, 0.0F, -13.0F, 6.0F, 11.0F, 13.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -6.5F, -15.5F));
+
+		PartDefinition head = neck.addOrReplaceChild("head", CubeListBuilder.create().texOffs(90, 111).addBox(-3.5F, 0.0F, -16.0F, 7.0F, 3.0F, 8.0F, new CubeDeformation(0.0F))
+				.texOffs(88, 70).addBox(-4.5F, 0.0F, -8.0F, 9.0F, 3.0F, 8.0F, new CubeDeformation(0.01F))
+				.texOffs(102, 96).addBox(-2.5F, 2.5F, -15.0F, 5.0F, 1.0F, 8.0F, new CubeDeformation(0.0F))
+				.texOffs(28, 114).addBox(-4.5F, 3.0F, -4.0F, 9.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, -5.0F));
+
+		PartDefinition jaw = head.addOrReplaceChild("jaw", CubeListBuilder.create().texOffs(60, 111).addBox(-3.5F, 0.0F, -12.0F, 7.0F, 4.0F, 8.0F, new CubeDeformation(0.01F))
+				.texOffs(112, 114).addBox(-2.5F, -0.5F, -11.0F, 5.0F, 1.0F, 8.0F, new CubeDeformation(0.0F))
+				.texOffs(116, 81).addBox(-4.5F, 0.0F, -4.0F, 9.0F, 4.0F, 4.0F, new CubeDeformation(0.01F)), PartPose.offset(0.0F, 3.0F, -4.0F));
+
+		PartDefinition tongue = jaw.addOrReplaceChild("tongue", CubeListBuilder.create().texOffs(11, 54).addBox(-1.5F, -0.01F, -9.0F, 3.0F, 0.0F, 9.0F, new CubeDeformation(0.0025F)), PartPose.offset(0.0F, 0.0F, -1.0F));
+
+		PartDefinition tail1 = body.addOrReplaceChild("tail1", CubeListBuilder.create().texOffs(0, 46).addBox(-5.5F, -4.0F, -4.0F, 11.0F, 7.0F, 33.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 2.5F, 15.5F));
+
+		PartDefinition tail2 = tail1.addOrReplaceChild("tail2", CubeListBuilder.create().texOffs(60, 86).addBox(-4.0F, -2.5F, 0.0F, 8.0F, 5.0F, 20.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.5F, 29.0F));
+
+		PartDefinition tail3 = tail2.addOrReplaceChild("tail3", CubeListBuilder.create().texOffs(0, 86).addBox(-3.0F, -2.0F, 0.0F, 6.0F, 4.0F, 24.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.5F, 20.0F));
+
+		PartDefinition arm_control = body_upper.addOrReplaceChild("arm_control", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, -19.0F));
+
+		PartDefinition left_arm1 = arm_control.addOrReplaceChild("left_arm1", CubeListBuilder.create().texOffs(100, 0).addBox(-3.0F, -2.0F, -3.0F, 6.0F, 12.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(9.5F, 0.0F, 0.0F));
+
+		PartDefinition left_arm2 = left_arm1.addOrReplaceChild("left_arm2", CubeListBuilder.create().texOffs(100, 36).addBox(-3.5F, 0.0F, -5.0F, 8.0F, 3.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.5F, 9.0F, -1.0F));
+
+		PartDefinition right_arm1 = arm_control.addOrReplaceChild("right_arm1", CubeListBuilder.create().texOffs(100, 0).mirror().addBox(-3.0F, -2.0F, -3.0F, 6.0F, 12.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-9.5F, 0.0F, 0.0F));
+
+		PartDefinition right_arm2 = right_arm1.addOrReplaceChild("right_arm2", CubeListBuilder.create().texOffs(100, 36).mirror().addBox(-4.5F, 0.0F, -5.0F, 8.0F, 3.0F, 7.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-0.5F, 9.0F, -1.0F));
+
+		PartDefinition leg_control = body_main.addOrReplaceChild("leg_control", CubeListBuilder.create(), PartPose.offset(0.0F, 3.5F, 9.5F));
+
+		PartDefinition left_leg1 = leg_control.addOrReplaceChild("left_leg1", CubeListBuilder.create().texOffs(100, 18).addBox(-3.0F, -2.0F, -3.0F, 6.0F, 12.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(9.5F, 0.0F, 0.0F));
+
 		PartDefinition left_leg2 = left_leg1.addOrReplaceChild("left_leg2", CubeListBuilder.create().texOffs(0, 115).addBox(-4.0F, 0.0F, -5.0F, 8.0F, 3.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 9.0F, -1.0F));
-		PartDefinition right_leg1 = body_main.addOrReplaceChild("right_leg1", CubeListBuilder.create().texOffs(4, 10).mirror().addBox(-3.0F, -2.0F, -3.0F, 6.0F, 12.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-9.5F, 3.5F, 9.5F));
+
+		PartDefinition right_leg1 = leg_control.addOrReplaceChild("right_leg1", CubeListBuilder.create().texOffs(100, 18).mirror().addBox(-3.0F, -2.0F, -3.0F, 6.0F, 12.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-9.5F, 0.0F, 0.0F));
+
 		PartDefinition right_leg2 = right_leg1.addOrReplaceChild("right_leg2", CubeListBuilder.create().texOffs(0, 115).mirror().addBox(-4.0F, 0.0F, -5.0F, 8.0F, 3.0F, 7.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 9.0F, -1.0F));
-		return LayerDefinition.create(meshdefinition, 128, 128);
+
+		return LayerDefinition.create(meshdefinition, 256, 256);
 	}
 
 	@Override
-	public void setupAnim(Megalania entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 		this.root().getAllParts().forEach(ModelPart::resetPose);
 
-		this.animateWalk(MegalaniaAnimations.WALK, limbSwing, limbSwingAmount, 5, 8);
-		if (entity.isStillEnough()) {
-			this.animate(entity.idleAnimationState, MegalaniaIdleAnimations.IDLE, ageInTicks);
+		if (entity.getDeltaMovement().horizontalDistance() > 1.0E-5F) {
+			this.animateWalk(MegalaniaAnimations.WALK, limbSwing, limbSwingAmount, 5, 8);
 		}
+		this.animate(entity.idleAnimationState, MegalaniaAnimations.IDLE, ageInTicks);
 
 		this.head.xRot += headPitch * ((float) Math.PI / 180f) - (headPitch * ((float) Math.PI / 180f)) / 2;
 		this.head.yRot += netHeadYaw * ((float) Math.PI / 180f) - (netHeadYaw * ((float) Math.PI / 180f)) / 2;
@@ -116,7 +128,7 @@ public class MegalaniaModel<T extends Megalania> extends HierarchicalModel<T> {
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		root.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		this.root.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 
 	@Override
--- src/main/java/com/unusualmodding/unusual_prehistory/entity/Megalania.java
+++ src/main/java/com/unusualmodding/unusual_prehistory/entity/Megalania.java
@@ -1,5 +1,6 @@
 package com.unusualmodding.unusual_prehistory.entity;
 
+import com.unusualmodding.unusual_prehistory.entity.base.PrehistoricMob;
 import com.unusualmodding.unusual_prehistory.registry.UP2SoundEvents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
@@ -14,20 +15,17 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
-import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class Megalania extends Animal {
+public class Megalania extends PrehistoricMob {
 
     public final AnimationState idleAnimationState = new AnimationState();
 
-    private int idleAnimationTimeout = 0;
-
-    public Megalania(EntityType<? extends Animal> entityType, Level level) {
+    public Megalania(EntityType<? extends Megalania> entityType, Level level) {
         super(entityType, level);
         this.setMaxUpStep(1);
     }
@@ -35,39 +33,31 @@ public class Megalania extends Animal {
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 6.0F));
-        this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1));
-        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
+        this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 1));
+        this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6.0F));
+        this.targetSelector.addGoal(0, new HurtByTargetGoal(this));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 60.0D).add(Attributes.MOVEMENT_SPEED, 0.14F);
     }
 
-    @Override
-    public void tick () {
-        super.tick();
-        if (this.level().isClientSide()) {
-            this.setupAnimationStates();
-        }
-    }
-
-    private void setupAnimationStates() {
+    public void setupAnimationStates() {
         if (this.idleAnimationTimeout == 0) {
             this.idleAnimationTimeout = 320;
             this.idleAnimationState.start(this.tickCount);
         } else {
             --this.idleAnimationTimeout;
         }
-    }
-
-    public boolean isStillEnough() {
-        return this.getDeltaMovement().horizontalDistance() < 0.01;
+        if (this.getDeltaMovement().horizontalDistance() > 1.0E-5F) {
+            this.idleAnimationState.stop();
+            this.idleAnimationTimeout = 0;
+        }
     }
 
     @Override
-    public @Nullable AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
+    public @Nullable AgeableMob getBreedOffspring(ServerLevel serverLevel, AgeableMob mob) {
         return null;
     }
 
